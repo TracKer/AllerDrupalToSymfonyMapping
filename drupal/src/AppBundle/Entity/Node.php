93c09f84e06997105de188fc4634870b4d3e02a4
Andrey Rudenko <andrey.rudenko@gmail.com>
2015-11-28 12:59:15 +0200

    Update Node entity.

--- drupal/src/AppBundle/Entity/Node.php
+++ drupal/src/AppBundle/Entity/Node.php
@@ -34,6 +34,75 @@ class Node
    */
   private $type;
 
+  /**
+   * @var string
+   *
+   * @ORM\Column(name="language", type="string", length=12)
+   */
+  private $language;
+
+  /**
+   * @var string
+   *
+   * @ORM\Column(name="title", type="string", length=12)
+   */
+  private $title;
+
+  /**
+   * @return int
+   */
+  public function getVid() {
+    return $this->vid;
+  }
+
+  /**
+   * @param int $vid
+   */
+  public function setVid($vid) {
+    $this->vid = $vid;
+  }
+
+  /**
+   * @return string
+   */
+  public function getType() {
+    return $this->type;
+  }
+
+  /**
+   * @param string $type
+   */
+  public function setType($type) {
+    $this->type = $type;
+  }
+
+  /**
+   * @return string
+   */
+  public function getLanguage() {
+    return $this->language;
+  }
+
+  /**
+   * @param string $language
+   */
+  public function setLanguage($language) {
+    $this->language = $language;
+  }
+
+  /**
+   * @return string
+   */
+  public function getTitle() {
+    return $this->title;
+  }
+
+  /**
+   * @param string $title
+   */
+  public function setTitle($title) {
+    $this->title = $title;
+  }
 
   /**
    * Get id
